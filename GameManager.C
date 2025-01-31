323e5dc70896b88af1b4f64b87743c06e09be421
stefsonsampaio <stefsonsampaio01@gmail.com>
2025-01-31 13:21:39 -0300

    iniciando estruturação de classes e funções

--- GameManager.C
+++ GameManager.C
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int vida;
+    int ataque;
+    int defesa;
+    int quantidade_cura;
+} Player;
+
+typedef struct {
+    int vida;
+    int ataque;
+} Inimigo;
+
+Player inicializaPlayer(int vida, int ataque, int defesa, int quantidade_cura) {
+    Player p;
+    p.vida = vida;
+    p.ataque = ataque;
+    p.defesa = defesa;
+    p.quantidade_cura = quantidade_cura;
+
+    return p;
+}
+
+void exibePropriedadesDoPlayer(Player *p) {
+    printf("vida atual do player: %d\n", p->vida);
+}
 
 int main()
 {
-    printf("Hello World");
+    Player meuPlayer = inicializaPlayer(100, 10, 10, 3);
+
+    exibePropriedadesDoPlayer(&meuPlayer);
 
     return 0;
 }
