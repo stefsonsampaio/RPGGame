e27c6e3a9b11f29b2a12a8a7616a408f4b1c59a9
stefsonsampaio <stefsonsampaio01@gmail.com>
2025-02-24 08:33:45 -0300

    adicionando comentários conforme exigência da avaliação.

--- GameManager.C
+++ GameManager.C
@@ -1,6 +1,7 @@
 //comando para executar script no CMD
 //cd "c:\vs.code\RPGGame" && gcc GameManager.C -o GameManager && "C:\vs.code\RPGGame\"GameManager 
 
+//includes
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,6 +10,7 @@
 #include <math.h>
 #include <windows.h>
 
+//enum utilizado para cada capitulo do jogo
 typedef enum {
 	VilaInicial,
 	Castelo,
@@ -17,6 +19,7 @@ typedef enum {
 	CidadeDosGoblins
 } Ambiente;
 
+//enum utilizado para definicao de tipo de item equipado pelo player
 typedef enum {
     Nenhum = -1,
     Defesa,
@@ -24,26 +27,31 @@ typedef enum {
     Magia
 } TipoItem;
 
+//declaracao classe de item equipado do player
 typedef struct {
     char nome[50];
     TipoItem tipo;
     int valor;
 } Item;
 
+//declaracao da classe player
 typedef struct {
     int vida;
     int ataque;
     int defesa;
-    int quantidade_cura;
+    int quantidade_cura; //o player inicia com 3 potes de cura
+	int segunda_chance; //o player inicia com 3 "segundas chances" para caso morra sem concluir o jogo
     Item item;
 } Player;
 
+//declaracao da classe inimigo
 typedef struct {
     int vida;
     int ataque;
     int level;
 } Inimigo;
 
+//funcao para inicializar player
 Player inicializaPlayer(int vida, int ataque, int defesa, int quantidade_cura) {
     Player p;
     p.vida = vida;
@@ -54,16 +62,20 @@ Player inicializaPlayer(int vida, int ataque, int defesa, int quantidade_cura) {
     strcpy(p.item.nome, "");
     p.item.tipo = Nenhum;
     p.item.valor = 0;
+	
+	p.segunda_chance = 3;
 
     return p;
 }
 
+//funcao para equipar item no player
 void equiparItem(Player *p, const char *nome, TipoItem tipo, int valor) {
     strcpy(p->item.nome, nome);
     p->item.tipo = tipo;
     p->item.valor = valor;
 }
 
+//funcao para inicializar inimigo1
 Inimigo inicializaInimigo(int nivel) {
     Inimigo i;
     i.vida = (int)ceil(40 * nivel);
@@ -73,6 +85,8 @@ Inimigo inicializaInimigo(int nivel) {
     return i;
 }
 
+//funcao para printar propriedades atuais do player
+//essa funcao é utilizada repetidamente no decorrer do jogo
 void exibePropriedadesDoPlayer(Player *p) {
     printf("\nSuas propriedades:\n");
     printf("- vida: %d\n", p->vida);
@@ -88,26 +102,36 @@ void exibePropriedadesDoPlayer(Player *p) {
             printf("- Defesa do equipamento: %d\n", p->item.valor);
         }
     }
+	printf("- quantidade de segundas chances restantes: %d\n", p->segunda_chance);
     Sleep(1200);
 }
 
+//funcao para luta
+//essa funcao tem um comportamento isolado da funcao base que roda o jogo
+//essa funcao é chamada na rodada inicial e a cada 4 rodadas do jogo
 void luta(Player *p, Inimigo *i) {
+	//para garantir que a funcao random funcione corretamente
     srand(time(NULL));
 
     printf("\nInicio de batalha!\n");
     Sleep(1200);
     
+	//printando level do inimigo de acordo com a rodada atual
     printf("Voce deu de cara com um goblin level %d, de %d pontos de vida!\n", i->level, i->vida);
 
+	//inicializando variável utilizada para definir se a defesa está ativa
     int rodadasComEscudoAtivo;
 
+	//loop utilizado para a luta player x inimigo
     while(p->vida > 0 && i->vida > 0) {
+		//verificando se o escudo permanece ativo
         if (rodadasComEscudoAtivo > 0) {
             Sleep(1200);
             printf("Seu escudo ainda esta ativo por %d rodadas!\n", rodadasComEscudoAtivo);
         }
 
         Sleep(1200);
+		//inicializando variável usada para capturar como o player vai prosseguir
         int escolha;
         do {
             printf("\n1. Atacar\n2. Defender\n3. Usar Cura\nEscolha: ");
@@ -118,7 +142,7 @@ void luta(Player *p, Inimigo *i) {
         } while (escolha < 1 || escolha > 3);
 
         if (escolha == 1) {
-            int danoPlayer = (p->ataque - 5) + (rand() % 6);
+            int danoPlayer = (p->ataque - 5) + (rand() % 6); //o dano do player é random e varia de acordo com o item equipado
             if (p->item.tipo == Ataque) {
                 danoPlayer += p->item.valor;
             }
@@ -134,16 +158,18 @@ void luta(Player *p, Inimigo *i) {
                 if (i->vida <= 0) {
                     Sleep(1200);
                     printf("\nO inimigo foi derrotado!\n");
-                    break;
+                    break; //a batalha acama se o inimigo morrer
                 }
             }
         }
         else if (escolha == 2) {
+			//se escolher usar a defesa, a defesa do player ganha um buf foi 3 "rodadas" no loop
             Sleep(1200);
             printf("Defesa reforcada por 3 rodadas!\n");
             rodadasComEscudoAtivo = 3;
         }
         else if (escolha == 3) {
+			//se utilizar cura, e ainda tiver item de cura no estoque, a vida do player aumenta
             if (p->quantidade_cura > 0) {
                 p->vida += 20;
                 p->quantidade_cura -= 1;
@@ -158,7 +184,7 @@ void luta(Player *p, Inimigo *i) {
         }
 
         int danoInimigo = ((i->ataque - 5) + (rand() % 6)) - p->defesa;
-        if (p->item.tipo == Defesa && rodadasComEscudoAtivo > 0) {
+        if (p->item.tipo == Defesa && rodadasComEscudoAtivo > 0) { // o dano do inimigo também é random, e diminui se o player estiver com o escudo ativo na rodada
             danoInimigo -= p->item.valor;
         }
         
@@ -177,12 +203,13 @@ void luta(Player *p, Inimigo *i) {
             }
         }
 
-        if (rodadasComEscudoAtivo > 0) {
+        if (rodadasComEscudoAtivo > 0) { //subtraindo a quantidade de rodadas com escudo ativo
             rodadasComEscudoAtivo -= 1;
         }
     }
 }
 
+//funcao para facilitar a definicao do ambiente atual do player
 Ambiente obterAmbiente(int nivel) {
 	if (nivel <= 3) return VilaInicial;
 	else if (nivel <= 7) return Castelo;
@@ -191,6 +218,7 @@ Ambiente obterAmbiente(int nivel) {
     else return CidadeDosGoblins;
 }
 
+//funcao para possibilitar a exploracao em diferentes ambientes
 void explorar(Player *p, int rodada) {
     Ambiente ambiente = obterAmbiente(rodada);
     int opcao;
@@ -271,13 +299,16 @@ int main()
     printf("Por ultimo, vale ressaltar que tanto voce quanto o inimigo tem um sistema de level e upgrade, e ambos ficam mais fortes de acordo com o level.\n");
     printf("\nDito isso, valos comecar o jogo!\n");
 
+	//variável usada somente para dar uma pausa antes de iniciar de fato o jogo
     char temp;
     printf("\nPressione qualquer tecla para continuar...\n");
-    scanf("%c", temp);
+    scanf(" %c", &temp);
 
+	//inicializacao do player
     Player meuPlayer = inicializaPlayer(100, 20, 4, 3);
 
-    for (int i = 0; i < 5; i++) {
+	//loop inicial do game. O jogo tem 20 rodadas
+    for (int i = 0; i < 20; i++) {
         printf("\nInicio da rodada %d!\n", i + 1);
         Sleep(1200);
 
@@ -290,11 +321,12 @@ int main()
         }
 
         Sleep(1200);
+		//exibindo propeirdades atuais do player
         exibePropriedadesDoPlayer(&meuPlayer);
 
 
         printf("\nPronto para a proxima etapa? (S/N)\n");
-        fflush(stdin);
+        fflush(stdin); //evitar bugs ao capturar resposta do player
         scanf(" %c", &nextLevel);
 
         if (toupper(nextLevel) == 'N') {
@@ -303,6 +335,7 @@ int main()
         }
 
         if (((i + 1) % 4 == 0) || i == 0) {
+			//o inimigo é inicializado a cada 4 rodadas e suas propriedades aumentam de acordo com o level atual
             Inimigo meuInimigo = inicializaInimigo(i > 0 ? (i + 1) / 4 : 1);
             luta(&meuPlayer, &meuInimigo);
         }
@@ -311,6 +344,7 @@ int main()
         }
     }
 
+	//Se chegar ao fim do jogo vivo, significa que venceu o jogo
     if (meuPlayer.vida > 0) {
         Sleep(1200);
         printf("\nParabens! Voce venceu o jogo!\n");
